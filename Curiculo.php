10918c0fba8f9d45049bc971f7f6ed8eadddfaeb
MenteSuja <42467879+MenteSuja@users.noreply.github.com>
2019-03-11 18:08:56 +0000

    primeiro

---
+++ Curiculo.php
@@ -0,0 +1,81 @@
+<!DoCTYPE html>
+
+<html lang="Pt-pt">
+
+
+<HEAD>
+
+<meta charset="utf-8">
+<TITLE> Elton Julio Curriculum </TITLE>
+
+</HEAD>
+
+
+
+
+<body style="color:#181407;font-family: verdana,geneva,tahoma,Sans-serif; font-weight: normal;">
+	<figure style="float: left;"> 
+
+	<img src="eu.jpg;"> 
+
+	<figcaption></figcaption>
+
+	 </figure> 
+<DIV style ="margin-right:auto; :margin-left: auto;width: 1060px;background-color: #00FFFF;border-bottom: double;">
+
+
+
+<h3 style="font-size:20pt;">Elton Julio Cuambe</h3>
+<h5 style=" font-size: 10pt;">Estudante de Licenciatura em Informática</h5>
+
+
+
+</DIV>
+<div style="background-color:#d77b68;" >
+	<h2>Dados Pessoais</h2>
+<span>Idade: 22 </span> <br>
+<span>Estado Civil: Solteiro</span>
+
+</div>
+<br>
+<div style ="background-color:#847b68;">
+
+<h2>Formação</h2>
+			
+			<p><label >Ens Primário(1ª a 7ª)</label>Escola primária Completa Kurhula: 2003-2009 </p>
+			<p><label>Ens Secundario(1º Ciclo)</label>Escola Secundaria Noroeste 2: 2010-2013</p>
+			<p><label>Ens Secundario(2º Ciclo)</label>Escola Secundaria da Polana: 2014-2015</p>
+</div>
+<div style =" background-color:#d7a568;">
+<h2>Linguas</h2>
+<ul>
+<li>Excelente na lingua portuguesa em nivel de escrita, fala e percepcao</li>
+<li>Razoavel na lingua Inglesa em nivel de escrita, fala e percepcao</li>
+<ul>
+
+</div>
+
+<div style ="background-color:#d7a5ab;">
+
+<h2>Habilidades</h2>
+<ul>
+	<li> Boa organizacao e responsabilidade</li>
+	<li>Capaz de trabalhar em equipe</li>
+	<li>Bom relacionamennto com colegas</li>
+    <li>Disponibilidade imediata</li>
+</ul>
+
+</div>
+<div style ="Background-color:#468c0a">
+<h2>Contactos</h2>
+<ul>
+<li>842078450</li>
+<li>Eltonnjulio@Outlook.com</li>
+</ul>
+
+</div>
+</body>
+
+
+
+</html>
